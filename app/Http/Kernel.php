07c24c18a9864c549985c8540e967ec16c604d79
Toby Twigger <tobytwigger1@gmail.com>
2023-02-17 01:55:39 +0000

    Throttle api less

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            'throttle:500,60',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
