7ff48415591fbded26a11b36dc6dd83e4c85b2fa
Toby Twigger <tobytwigger1@gmail.com>
2023-02-19 02:52:09 +0000

    Add trimming

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('job-status:clear --preserve=12')->everyFifteenMinutes();
-        // $schedule->command('inspire')->hourly();
     }
 
     /**
